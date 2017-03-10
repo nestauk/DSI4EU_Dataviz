84cc38e47fb6d99dad62ec735e0c4f92bb10413d
abusedmedia <ff@todo.to.it>
2017-03-10 18:04:34 +0100

    small fix

--- grunt/config/string-replace.js
+++ grunt/config/string-replace.js
@@ -7,6 +7,30 @@ module.exports = function (grunt, options) {
   // this is the grunt configuration object
   return {
 
+  	ga: {
+	    files: [{
+	      expand: true,
+	      cwd: options.folders.dist,
+	      src: '**/*.html',
+	      dest: options.folders.dist
+	    }],
+	    options: {
+	      replacements: [{
+	        pattern: /<!-- ganalytics:(.*) -->/g,
+	        replacement: function(match, p){
+	        	return  "<script>"+
+						"	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){"+
+						"	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),"+
+						"	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)"+
+						"	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');"+
+						"	  ga('create', '"+p+"', 'auto');"+
+						"	  ga('send', 'pageview');"+
+						"</script>"
+	        }
+	      }]
+	    }
+	},
+
   	dist: {
 	    files: [{
 	      expand: true,
