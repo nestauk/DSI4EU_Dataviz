b2166f4a18570bf8ba25804d49e287ad9ec48531
Emilio <e.bondioli@todo.to.it>
2017-03-10 17:15:37 +0100

    Init global APP object and Dataset module

--- app/js/app.js
+++ app/js/app.js
@@ -1,49 +1,46 @@
 ;(function(window, $, undefined){
-	
-	console.log('app');
 
-
+	window.APP = {}
+	APP.dataset = new Dataset();	
 
 	$(document).ready(function(){
 
-    var timeline = ["loaderState", "onboardingState", "mapState", "networkState", "clusterState", "shareState"];
-    var currentStep = 0;
+	    var timeline = ["loaderState", "onboardingState", "mapState", "networkState", "clusterState", "shareState"];
+	    var currentStep = 0;
 
-    setTimeout(function(){
-      stator.go("loaderState", {encode: false})
-    })
+	    APP.stator.go("loaderState", {encode: false})
 
-    stator.on("moveForward", moveForward);
-    stator.on("moveBackward", moveBackward);
+	    APP.stator.on("moveForward", moveForward);
+	    APP.stator.on("moveBackward", moveBackward);
 
-    function moveForward(){
-      window.stator.direction = "up";
-      if(currentStep < timeline.length-1){
-        stator.go(timeline[++currentStep], {encode: false});
-      }
-      console.log(currentStep);
-    }
+	    function moveForward(){
+	      APP.stator.direction = "up";
+	      if(currentStep < timeline.length-1){
+	        APP.stator.go(timeline[++currentStep], {encode: false});
+	      }
+	      console.log(currentStep);
+	    }
 
-    function moveBackward(){
-      window.stator.direction = "down";
-      if(currentStep > 0) {
-        stator.go(timeline[--currentStep], {encode: false});
-      }
-      console.log(currentStep);
-    }
+	    function moveBackward(){
+	      APP.stator.direction = "down";
+	      if(currentStep > 0) {
+	        APP.stator.go(timeline[--currentStep], {encode: false});
+	      }
+	      console.log(currentStep);
+	    }
 
 
-    // listeners
-    window.addEventListener("keydown", doKeyDown, true);
+	    // listeners
+	    window.addEventListener("keydown", doKeyDown, true);
 
-    function doKeyDown(e){
-      if(e.key === "ArrowRight") {
-        moveForward();
-      }
-      if(e.key === "ArrowLeft") {
-        moveBackward();
-      }
-    }
+	    function doKeyDown(e){
+	      if(e.key === "ArrowRight") {
+	        moveForward();
+	      }
+	      if(e.key === "ArrowLeft") {
+	        moveBackward();
+	      }
+	    }
 
   })
 
