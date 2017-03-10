df2e502d70e2bc977fbcdbd862bb362a8dfd62bc
Emilio <e.bondioli@todo.to.it>
2017-03-10 17:14:46 +0100

    Add basic Dataset functions

--- app/js/modules/dataset.js
+++ app/js/modules/dataset.js
@@ -1,19 +1,32 @@
 function Dataset(){
+	var self = this;
 	self.data = [];
-	self.load = loadData;
+	self.loadData = loadData;
+
+	const organisations_path = 'data/organisations.json';
+	const projects_path = 'data/projects.json';
 
 	// loads the .csv file and returns the data
-	function loadData(path, callback){
+	function loadData(callback){
+		d3.queue()
+		  .defer(d3.json, organisations_path)
+		  .defer(d3.json, projects_path)
+		  .await(function(error, orgData, prjData){
+		  	if(error) {
+		  		console.log(error);
+		  		return;
+		  	} 
+		  	prepareData({orgs: orgData, prjs: prjData});
+		  	if(callback) callback();
+		  });
 
 
-		if(callback) callback(data)
-		else return data;
 	}
 
 	// cleans up the data and adds additional fields to the items
-	function prepareData(){
-
-		return data;
+	function prepareData(data){
+		self.orgs = data.orgs;
+		self.prjs = data.prjs;
 	}
 
 	function cleanData(){
