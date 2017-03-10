68469d2887dd410d2283b6ffbe000617636a6c76
Emilio <e.bondioli@todo.to.it>
2017-03-10 17:17:57 +0100

    Load data on start

--- app/js/states/loaderState.js
+++ app/js/states/loaderState.js
@@ -4,24 +4,14 @@ function loaderState(){
 		enter: function(option){
 			console.log('loaderState :: enter');
 
-			d3.queue()
-			  .defer(d3.json, 'data/organisations.json')
-			  .defer(d3.json, 'data/projects.json')
-			  .await(handleData);
-
+			APP.dataset.loadData(function(){
+				APP.stator.go('onboardingState', {encode: false})
+			})
 		},
 		leave: function(option){
 			console.log('loaderState :: leave')
 			
 		}
-	}
-
-
-
-	function handleData(error, orgData, prjData) {
-
-		console.log("Organisations: ", orgData);
-		console.log("Projects: ", prjData);
 	}
 
 }
