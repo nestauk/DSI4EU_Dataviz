30d3215319c5395e6ccf5b89b18a2c6205eaaa6f
andreacorradi <a.corradi@todo.to.it>
2017-03-13 18:32:26 +0100

    network state added, still not working

--- app/js/states/networkState.js
+++ app/js/states/networkState.js
@@ -1,12 +1,18 @@
 function networkState(){
 
+	APP.network = new NetworkView();
+
 	return {
 		enter: function(option){
-			console.log('networkState :: enter')
+			console.log('networkState :: enter');
+
+			APP.network.create();
 
 		},
 		leave: function(option){
-			console.log('networkState :: leave')
+			console.log('networkState :: leave');
+
+			APP.network.delete();
 			
 		}
 	}
