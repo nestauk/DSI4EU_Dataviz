98404f231234d39c2c889e604d24dcfaaf5ea8a4
Emilio <e.bondioli@todo.to.it>
2017-03-14 15:02:26 +0100

    Add ui module

---
+++ app/js/modules/ui.js
@@ -0,0 +1,48 @@
+function UserInterface() {
+	var self = this;
+	self.updateNavigation = updateNavigation;
+	self.hide = hideUI
+	self.show = showUI
+
+	var nav_next = $('#nav-next');
+	var nav_current = $('#nav-current');
+	var nav_prev = $('#nav-prev');
+
+	// $('#user-interface').hide();
+
+	function updateNavigation(){
+		var nav_map = $('#nav-map');
+		var nav_network = $('#nav-network');
+		var nav_cluster = $('#nav-cluster');
+		switch(APP.state){
+			case "map":
+				nav_current.append(nav_map);
+				nav_next.prepend(nav_network);
+				nav_prev.append(nav_cluster);
+			break;
+			case "network":
+				nav_current.append(nav_network);
+				nav_next.prepend(nav_cluster);
+				nav_prev.append(nav_map);
+			break;
+			case "cluster":
+				nav_current.append(nav_cluster);
+				nav_next.prepend(nav_map);
+				nav_prev.append(nav_network);
+			break;
+			default:
+				nav_current.append(nav_map);
+				nav_next.prepend(nav_network);
+				nav_prev.append(nav_cluster);
+			break;
+		}
+	}
+
+	function hideUI(){
+		$('#user-interface').fadeOut();
+	}
+
+	function showUI(){
+		$('#user-interface').fadeIn();
+	}
+}
