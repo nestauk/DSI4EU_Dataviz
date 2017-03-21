39c7f2022f6aa21559961a636e7f9d91b1208b90
andreacorradi <a.corradi@todo.to.it>
2017-03-21 09:16:06 +0100

    cluster panel wip

--- app/js/modules/clusterpanel.js
+++ app/js/modules/clusterpanel.js
@@ -3,31 +3,47 @@ function ClusterPanel() {
 	self.fillHeader = fillHeader;
 	self.drawPanel = drawPanel;
 
-	function fillHeader(_selectedCluster) {
-		// var selectedData = APP.cluster.packdata.filter(function (d) {
-		// 	return d.name == "United Kingdom";
-		// })
-		console.log(_selectedCluster)
-		var prjCounts = d3.sum(_selectedCluster.values, function (d) {
+	function fillHeader(selectedCluster) {
+		var prjCounts = d3.sum(selectedCluster.values, function (d) {
 			return d.values.length;
 		})
-		$(".cluster-panel-container h2").html(_selectedCluster.key);
+		$(".cluster-panel-container h2").html(selectedCluster.key);
 		$(".cluster-panel-container .cluster-subtitle").html(prjCounts+" projects");
 	}
 
-	function drawPanel(_selectedCluster) {
-		// _selectedCluster.children.forEach(function (d) {
-		// 	console.log(d)
-		// 	var el = $(".cluster-panel-container .cluster-panel-scrolling ul").append("li")
-		// 	el.html(d.name+", "+d.size)
-		// })
-		d3.select(".cluster-panel-container .cluster-panel-scrolling ul").selectAll("li")
-			.data(_selectedCluster.values)
+	function drawPanel(selectedCluster) {
+
+		var items = d3.select(".cluster-panel-container .cluster-panel-scrolling ul").selectAll(".cluster-item")
+			.data(selectedCluster.values)
 			.enter()
 			.append("li")
-			.html(function (d) {
-				return "<strong>"+d.key+"</strong>: "+d.values.length;
+			.attr("class", "cluster-item")
+			.text(function (d) {
+				return d.key+": "+d.values.length;
 			})
+			.each(generatePrjList)
+			.on("click", clusterItemDetail)
+
+		items.on("click", clusterItemDetail);
+
+		function clusterItemDetail() {
+			$(".cluster-panel-scrolling .sub-list").toggleClass("invisible");
+		}
+
+		function generatePrjList(e, i) {
+			d3.select(this).append("ul")
+				.attr("class", "sub-list invisible")
+					.selectAll("sub-list-items")
+					.data(e.values)
+					.enter()
+					.append("li")
+						.attr("class", "sub-list-items")
+						.text(function (f) {
+							return f.name;
+						})
+		}
+
+
 	}
 
 }
