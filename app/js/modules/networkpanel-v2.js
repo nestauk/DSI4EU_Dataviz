fde29d8bb2e0d75c84ba026467da484f9c3f08d2
andreacorradi <a.corradi@todo.to.it>
2017-03-22 20:28:25 +0100

    network panel revisited: main items are prjs rather then orgs

---
+++ app/js/modules/networkpanel-v2.js
@@ -0,0 +1,52 @@
+function NetworkPanel() {
+	var self = this;
+	self.fillPanel = fillPanel;
+	self.deleteNetworkPanelItems = deleteNetworkPanelItems;
+
+	function fillPanel(selectedOrg) {
+		//console.log(selectedOrg);
+
+		$(".network-panel-title h2").text(selectedOrg.name);
+		$(".network-panel-subtitle").text(selectedOrg.shared_prjs.length+" shared projects");
+
+		selectedOrg.shared_prjs.forEach(function (o) {
+			var list = $(".network-panel-scrolling ul").get(0)
+			var li = $("<li></li>")
+				.addClass("network-panel-item")
+			list.append(li.get(0));
+
+			var titleDiv = $("<div></div>")
+				.addClass("network-panel-item-title")
+				.text(o.name)
+			li.get(0).append(titleDiv.get(0));
+
+			var barDiv = $("<div></div>")
+				.addClass("network-panel-item-bar")
+				.css("width", o.linked_orgs.length+"rem")
+			li.get(0).append(barDiv.get(0));
+
+			var orgsDiv = $("<div></div>")
+				.addClass("network-panel-item-org")
+				.text(createOrgList(o))
+			li.get(0).append(orgsDiv.get(0));
+		})
+		
+		function createOrgList(d) {
+			var orglist = "Shared with: ";
+			d.linked_orgs.forEach(function (l, i) {
+				if (l.name != selectedOrg.name) {
+					orglist = orglist.concat(l.name);
+					if (i != d.linked_orgs.length-1) orglist = orglist.concat(", ");
+				}
+			})
+			return orglist;
+		}
+
+	}
+
+
+	function deleteNetworkPanelItems() {
+		d3.select(".network-panel-scrolling ul").selectAll("li").remove();
+	}
+
+}
--- app/js/modules/networkpanel.js
+++ app/js/modules/networkpanel.js
@@ -4,16 +4,66 @@ function NetworkPanel() {
 	self.deleteNetworkPanelItems = deleteNetworkPanelItems;
 
 	function fillPanel(selectedOrg) {
-		console.log(selectedOrg);
+		//console.log(selectedOrg);
 
 		$(".network-panel-title h2").text(selectedOrg.name);
 		$(".network-panel-subtitle").text(selectedOrg.shared_prjs.length+" shared projects");
+
+		selectedOrg.linked_orgs.forEach(function (f) {
+			f.matchingSharedPrjs = _.intersectionBy(f.shared_prjs, selectedOrg.shared_prjs, "id");
+		})
+
+		var items = d3.select(".network-panel-scrolling ul").selectAll(".network-panel-item")
+			.data(selectedOrg.linked_orgs)
+				.enter()
+				.append("li")
+					.attr("class", "network-panel-item")
+
+		items.append("div")
+			.attr("class", "network-panel-item-title")
+			.text(function (d) {
+				return d.name;
+			})
+
+		items.append("div")
+			.attr("class", "network-panel-item-bar")
+			.style("width", function (d) {
+				return d.matchingSharedPrjs.length+"rem";
+			})
+
+		items
+			.each(generatePrjList)
+			.on("click", viewPrjList)
+		
+		function viewPrjList() {
+			if (d3.event.target == this || d3.event.target.parentNode == this) {
+				$(this).find("ul").toggleClass("invisible");
+			}
+		}
+
+		function generatePrjList(e, i) {
+			d3.select(this).append("ul")
+				.attr("class", "sub-list invisible")
+					.selectAll("sub-list-items")
+					.data(e.matchingSharedPrjs)
+					.enter()
+					.append("li")
+						.attr("class", "sub-list-items")
+						.append("a")
+							.attr("href", function (d) {
+								return d.url;
+							})
+							.attr("target", "_blank")
+							.text(function (f) {
+								return f.name;
+							})
+		}
 
 	}
 
 
 	function deleteNetworkPanelItems() {
-		
+		d3.select(".network-panel-scrolling ul").selectAll("li").remove();
 	}
 
 }
