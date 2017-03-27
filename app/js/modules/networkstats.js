4225716060f17cd784a23a81cc68726d38840b16
andreacorradi <a.corradi@todo.to.it>
2017-03-27 18:20:15 +0200

    network stats panel small upgrade

--- app/js/modules/networkstats.js
+++ app/js/modules/networkstats.js
@@ -9,34 +9,10 @@ function NetworkStats() {
 		var orgConnRatio = Math.round((connections.totalLinkedOrgs/APP.dataset.orgs.length)*100);
 		var prjShareRatio = Math.round((connections.totalSharedPrjs/APP.dataset.prjs.length)*100);
 		$(".network-stats-orgs .stats-caption").html("Networked organisations: "+orgConnRatio+"%");
-		$(".network-stats-orgs .stats-bar").transition({width: orgConnRatio+"%"}, 2000);
+		$(".network-stats-orgs .stats-bar .color").transition({width: orgConnRatio+"%"}, 2000);
 		$(".network-stats-prjs .stats-caption").html("Shared projects: "+prjShareRatio+"%");
-		$(".network-stats-prjs .stats-bar").transition({width: prjShareRatio+"%"}, 2000);
+		$(".network-stats-prjs .stats-bar .color").transition({width: prjShareRatio+"%"}, 2000);
 
-	}
-
-
-	function getNetwork(org){
-		var network_orgs = []
-		var network_prjs = []
-		network_orgs.push(org)
-
-		getOrgNetwork(org)
-		function getOrgNetwork(org){
-			var prjs = _.difference(org.shared_prjs, network_prjs);
-			network_prjs = network_prjs.concat(prjs)
-				prjs.forEach(function(p){
-					var orgs = _.difference(p.linked_orgs, network_orgs);
-					network_orgs = network_orgs.concat(orgs)
-						orgs.forEach(function(o){
-							getOrgNetwork(o)
-						})
-				})
-		}
-		return {
-			prjs: network_prjs,
-			orgs: network_orgs
-		}
 	}
 
 
