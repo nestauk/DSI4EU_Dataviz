47f074f536e51de2f5af0e1a06c3af011616e961
Emilio <e.bondioli@todo.to.it>
2017-03-27 17:10:43 +0200

    Remove commas from aliases

--- app/js/libs/utils.js
+++ app/js/libs/utils.js
@@ -1,6 +1,7 @@
 function idEncode(str, separator) {
 	if (!separator) separator = '_';
-	str = str.split("\"").join('').split('\'').join('');
+	str = _.deburr(str)
+	str = str.split("\"").join('').split('\'').join('').split(',').join('');
 	str = str.toLowerCase().split(' ').join(separator);
 	str = str.replace(/[ÀÁÂÃÄÅàáâãäåĀāąĄ]/g, "a");
 	str = str.replace(/[ÈÉÊËèéêëěĚĒēęĘ]/g, "e");
