981836bbd58de4efe72e635c7bd1cabd1440b29c
andreacorradi <a.corradi@todo.to.it>
2017-03-28 18:22:16 +0200

    cluster panel interaction fine tuning

--- app/js/modules/clusterpanel.js
+++ app/js/modules/clusterpanel.js
@@ -25,9 +25,6 @@ function ClusterPanel() {
 					.attr("class", "cluster-item")
 					.on("click", clusterItemDetail);
 
-		items.append("div")
-			.attr("class", "line")
-
 		var circleContainer = items.append("div")
 			.attr("class", "cluster-circle-container")
 
@@ -71,15 +68,33 @@ function ClusterPanel() {
 					.attr("xlink:href", "#down-icon")
 
 		cta.append("div")
+			.attr("class", "cta-text")
 			.style("display", "inline-block")
 			.text(function (d) {
-				return "Show "+d.values.length+" projects";
+				return "Show "+d.values.length+_.pluralize(" project", d.values.length);
 			})
 
 		cta.each(generatePrjList)
 		
 		function clusterItemDetail() {
-			$(this).find("ul").toggleClass("invisible");
+			var el = $(this).find("ul");
+			var listLength = $(this).find("ul li").length;
+			var elH = listLength*40;
+			el.toggleClass("invisible");
+			if (el.hasClass("invisible")) {
+				el.transition({height: 0}, 1000, "easeInQuint")
+				$(this).find(".down-icon").transition({scale: 1});
+				$(this).find(".cta-text").text(function () {
+			    return $(this).text().replace("Hide", "Show");
+				})
+			} else {
+				el.height(0);
+				el.transition({height: elH+"px"}, 1000, "easeOutQuint")
+				$(this).find(".down-icon").transition({scale: -1});
+				$(this).find(".cta-text").text(function () {
+			    return $(this).text().replace("Show", "Hide");
+				})
+			}
 			// if (d3.event.target == this) {
 			// 	$(this).find("ul").toggleClass("invisible");
 			// }
