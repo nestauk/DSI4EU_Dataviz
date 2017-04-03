31f455858f3ac8f8e37300a9c0fddc3d186a0275
andreacorradi <a.corradi@todo.to.it>
2017-04-03 17:21:58 +0200

    info panels style modification

--- app/js/modules/infopanel.js
+++ app/js/modules/infopanel.js
@@ -7,13 +7,13 @@ function InfoPanel() {
 		switch(state){
 			case "map":
 				var frag = $("<li><div class='legend-dot org-color'></div>Organisations</li>");
-				$(".modal-panel.info-panel.map .info-container .legenda ul").get(0).append(frag.get(0));
+				$(".info-panel.map .info-container .legenda ul").get(0).append(frag.get(0));
 			break;
 			case "network":
 				var frag1 = $("<li><div class='legend-dot org-color'></div>Organisations</li>");
 				var frag2 = $("<li><div class='legend-dot prj-color'></div>Projects</li>");
-				$(".modal-panel.info-panel.network .info-container .legenda ul").get(0).append(frag1.get(0));
-				$(".modal-panel.info-panel.network .info-container .legenda ul").get(0).append(frag2.get(0));
+				$(".info-panel.network .info-container .legenda ul").get(0).append(frag1.get(0));
+				$(".info-panel.network .info-container .legenda ul").get(0).append(frag2.get(0));
 			break;
 			case "cluster":
 				var subdivideField = APP.cluster.subdivide_field;
@@ -21,7 +21,7 @@ function InfoPanel() {
 					var subdivideData = APP.dataset.fields[APP.cluster.subdivide_field];
 					var fieldColorScale = APP.getColorScale(APP.cluster.subdivide_field);
 				} else var subdivideData = [{name: "Projects", color: "#ffad69"}]
-				var item = d3.select(".modal-panel.info-panel.cluster .info-container .legenda ul").selectAll(".legenda-item")
+				var item = d3.select(".info-panel.cluster .info-container .legenda ul").selectAll(".legenda-item")
 					.data(subdivideData)
 					.enter()
 					.append("li")
@@ -46,7 +46,7 @@ function InfoPanel() {
 	}
 
 	function deleteLegend(state) {
-		d3.selectAll(".modal-panel.info-panel."+state+" .info-container .legenda ul li").remove();
+		d3.selectAll(".info-panel."+state+" .info-container .legenda ul li").remove();
 	}
 
 }
