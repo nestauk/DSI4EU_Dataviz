e1b4489cd17d056aae109c96f10f59a8a18fc4b9
abusedmedia <ff@todo.to.it>
2017-04-03 19:33:46 +0200

    onboarding progress

--- app/js/modules/onboarding_canvas.js
+++ app/js/modules/onboarding_canvas.js
@@ -1,13 +1,13 @@
-function OnBoardingCanvas(svg, _app) {
-	var self = this;
-    self.init = init
+/* global d3 */
+;(function (window, $) {
+  function init (svg, _app) {
+    var self = this
+    self.init = begin
     self.stop = stop
     self.one = one
-    self.two = two
     self.three = three
     self.four = four
     self.five = five
-
 
     var cont = $('#onboarding-view')
     var w = cont.width()
@@ -15,173 +15,178 @@ function OnBoardingCanvas(svg, _app) {
 
     var s = Math.max(w, h)
 
-    
-
-
     var canvas = cont.find('canvas').get(0)
     canvas.width = w
     canvas.height = h
 
     var timer
+    var currentStep
 
     var ctx = canvas.getContext('2d')
-    var detachContainer = document.createElement('custom')
-    var dataCont = d3.select(detachContainer)
 
-	var orgs
+    var orgs
     var prjs
     var conn
 
     var mapx = d3.scaleLinear()
-        .domain([0, 500])
-        .range([0, w])
+            .domain([0, 500])
+            .range([0, w])
 
     var mapy = d3.scaleLinear()
-        .domain([0, 500])
-        .range([0, h])
+            .domain([0, 500])
+            .range([0, h])
 
-    var mapr = (w>h) ? mapy : mapx
+    var mapr = (w > h) ? mapy : mapx
 
-	function draw() {
-        var node
+    function draw () {
+      var node
 
-		ctx.fillStyle = "#fff";
-		ctx.rect(0,0,w,h);
-		ctx.fill();
+      ctx.fillStyle = '#fff'
+      ctx.rect(0, 0, w, h)
+      ctx.fill()
 
-        conn.each(function(d) {
-            node = d3.select(this);
-            ctx.beginPath();
-            ctx.strokeStyle = rgbObToStr( node.attr("stroke"), node.attr("opacity") )
-            ctx.moveTo(mapx(node.attr("x1")), mapy(node.attr("y1")));
-            ctx.lineTo(mapx(node.attr("x2")), mapy(node.attr("y2")));
-            ctx.stroke();
-            ctx.closePath();
-        });
+      if (currentStep >= 4) {
+        conn.each(function (d) {
+          node = d3.select(this)
+          ctx.beginPath()
+          ctx.strokeStyle = rgbObToStr(node.attr('stroke'), node.attr('opacity'))
+          ctx.moveTo(mapx(node.attr('x1')), mapy(node.attr('y1')))
+          ctx.lineTo(mapx(node.attr('x2')), mapy(node.attr('y2')))
+          ctx.stroke()
+          ctx.closePath()
+        })
+      }
 
-		orgs.each(function(d) {
-		  	node = d3.select(this);
-		  	ctx.beginPath();
-		  	ctx.fillStyle = rgbObToStr( node.attr("fill"), node.attr("opacity") )
-		  	ctx.arc( mapx(node.attr("cx")), mapy(node.attr("cy")), mapr(node.attr("r")), 0, Math.PI*2);
-		  	ctx.fill();
-		  	ctx.closePath();
-		});
-        prjs.each(function(d) {
-            node = d3.select(this);
-            ctx.beginPath();
-            ctx.fillStyle = rgbObToStr( node.attr("fill"), node.attr("opacity") )
-            ctx.arc( mapx(node.attr("cx")), mapy(node.attr("cy")), mapr(node.attr("r")), 0, Math.PI*2);
-            ctx.fill();
-            ctx.closePath();
-        });
-        
-	}
+      if (currentStep >= 3) {
+        orgs.each(function (d) {
+          node = d3.select(this)
+          ctx.beginPath()
+          ctx.fillStyle = rgbObToStr(node.attr('fill'), node.attr('opacity'))
+          ctx.arc(mapx(node.attr('cx')), mapy(node.attr('cy')), mapr(node.attr('r')), 0, Math.PI * 2)
+          ctx.fill()
+          ctx.closePath()
+        })
+      }
 
-
-
-
-
-
-
-	function one(){
-        orgs.transition()
-            .duration(2000)
-            .delay(function(d, i){
-                return Math.random()*5000
-            })
-            .ease(d3.easeExp)
-            .attr('opacity', 1)
-            .attr('r', function(){
-                return d3.select(this).attr('r') / 5
-            })
-
-	}
-
-
-	function two(){
-        prjs.transition()
-            .duration(2000)
-            .delay(function(d, i){
-                return Math.random()*5000
-            })
-            .ease(d3.easeExp)
-            .attr('opacity', 1)
-            .attr('r', function(){
-                return d3.select(this).attr('r') / 5
-            })
-	}
-
-    function three(){
-        conn.transition()
-            .duration(3000)
-            .delay(function(d, i){
-                return Math.random()*5000
-            })
-            .attr('opacity', 1)
-            .transition()
-            .duration(2000)
-            .attr('opacity', .2)
-
+      prjs.each(function (d) {
+        node = d3.select(this)
+        ctx.beginPath()
+        ctx.fillStyle = rgbObToStr(node.attr('fill'), node.attr('opacity'))
+        ctx.arc(mapx(node.attr('cx')), mapy(node.attr('cy')), mapr(node.attr('r')), 0, Math.PI * 2)
+        ctx.fill()
+        ctx.closePath()
+      })
     }
 
-    function four(){
-        enterElements('prj', dataset.prjs)
+    function one () {
+      currentStep = 1
+      prjs.attr('cx', function () {
+        return Math.random() * w
+      })
+        .attr('cy', function () {
+          return Math.random() * h
+        })
+        .transition()
+        .duration(15000)
+        .delay(function (d, i) {
+          return Math.random() * 15000
+        })
+        .ease(d3.easeExp)
+        .attr('opacity', 1)
+        .attr('cx', function () {
+          return d3.select(this).attr('fcx')
+        })
+        .attr('cy', function () {
+          return d3.select(this).attr('fcy')
+        })
     }
 
-    function five(){
-        enterElements('prj', dataset.prjs)
+    function three () {
+      currentStep = 3
+      orgs.transition()
+                .duration(5000)
+                .delay(function (d, i) {
+                  return Math.random() * 10000
+                })
+                .ease(d3.easeExp)
+                .attr('opacity', 1)
+                .attr('r', function () {
+                  return d3.select(this).attr('r') / 5
+                })
     }
 
-    function init(){
-
-        var svg = d3.select('#onboarding-view svg')
-            .style('width', s)
-            .style('height', s)
-            .style('display', 'none')
-
-        orgs = svg.selectAll('#org > *')
-            .attr('opacity', 0)
-            .attr('r', function(){
-                var r = (d3.select(this).attr('r')>0) ? d3.select(this).attr('r') : d3.select(this).attr('rx')
-                return r * 5
-            })
-
-        prjs = svg.selectAll('#prj > *')
-            .attr('opacity', 0)
-            .attr('r', function(){
-                var r = (d3.select(this).attr('r')>0) ? d3.select(this).attr('r') : d3.select(this).attr('rx')
-                return r * 5
-            })
-
-        conn = svg.selectAll('#conn > *')
-            .attr('stroke', '#111111')
-            .attr('opacity', 0)
-
-        timer = d3.timer(draw);
+    function four () {
+      currentStep = 4
+      conn.transition()
+                .duration(3000)
+                .delay(function (d, i) {
+                  return Math.random() * 5000
+                })
+                .attr('opacity', 1)
+                .transition()
+                .duration(2000)
+                .attr('opacity', 0.2)
     }
 
-    function stop(){
-        timer.stop()
+    function five () {
+      currentStep = 5
     }
 
+    function begin () {
+      var svg = d3.select('#onboarding-view svg')
+                .style('width', s)
+                .style('height', s)
+                .style('display', 'none')
 
-    function hexToRgb(hex) {
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        } : null;
+      orgs = svg.selectAll('#org > *')
+                .attr('opacity', 0)
+                .attr('r', function () {
+                  var r = (d3.select(this).attr('r') > 0) ? d3.select(this).attr('r') : d3.select(this).attr('rx')
+                  return r * 5
+                })
+
+      prjs = svg.selectAll('#prj > *')
+                .attr('opacity', 0)
+                .attr('r', function () {
+                  var r = (d3.select(this).attr('r') > 0) ? d3.select(this).attr('r') : d3.select(this).attr('rx')
+                  return r
+                })
+                .attr('fcx', function () {
+                  return d3.select(this).attr('cx')
+                })
+                .attr('fcy', function () {
+                  return d3.select(this).attr('cy')
+                })
+
+      conn = svg.selectAll('#conn > *')
+                .attr('stroke', '#777')
+                .attr('opacity', 0)
+
+      timer = d3.timer(draw)
     }
 
-    function rgbObToStr(hex, a){
-        var rgb = hexToRgb(hex)
-        var str = ''
-        if(rgb){
-            str = 'rgba(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ',' + a + ')'
-        }
-        return str
+    function stop () {
+      timer.stop()
     }
 
-}
+    function hexToRgb (hex) {
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : null
+    }
+
+    function rgbObToStr (hex, a) {
+      var rgb = hexToRgb(hex)
+      var str = ''
+      if (rgb) {
+        str = 'rgba(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ',' + a + ')'
+      }
+      return str
+    }
+  }
+
+  window.OnBoardingCanvas = init
+})(window, jQuery)
