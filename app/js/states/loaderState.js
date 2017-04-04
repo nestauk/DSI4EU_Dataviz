ccdaa79837bfc7f3c2984d9bc0fb8da1cd0e7bb1
marcoaimo <marcoaimo.am@gmail.com>
2017-04-04 17:01:55 +0200

    add loader view

--- app/js/states/loaderState.js
+++ app/js/states/loaderState.js
@@ -1,11 +1,15 @@
 function loaderState(){
 
+  var body = $('#loader-view')
 	return {
 		enter: function(option){
 			console.log('loaderState :: enter');
-		},
-		leave: function(option){
-			console.log('loaderState :: leave');			
+      body.show()
+      body.transition({ y: '0%', delay: 500 }, 1200, 'easeOutQuart')
+    },
+    leave: function(option){
+      console.log('loaderState :: leave');
+      body.transition({ y: '-100%', delay: 500, complete: function() { body.hide() } }, 1200, 'easeOutQuart')
 		}
 	}
 
