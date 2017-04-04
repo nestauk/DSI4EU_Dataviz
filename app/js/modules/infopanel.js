fb0d2b0f94faca8615d11b3aeb3ccd35908f5c78
andreacorradi <a.corradi@todo.to.it>
2017-04-04 17:14:27 +0200

    networkStats embedded into network info panel completed

--- app/js/modules/infopanel.js
+++ app/js/modules/infopanel.js
@@ -10,10 +10,6 @@ function InfoPanel() {
 				$(".info-panel.map .info-container .legenda ul").get(0).append(frag.get(0));
 			break;
 			case "network":
-				// var frag1 = $("<li><div class='legend-dot org-color'></div><div class='legend-text'>Organisations</div></li>");
-				// var frag2 = $("<li><div class='legend-dot prj-color'></div><div class='legend-text'>Projects</div></li>");
-				// $(".info-panel.network .info-container .legenda ul").get(0).append(frag1.get(0));
-				// $(".info-panel.network .info-container .legenda ul").get(0).append(frag2.get(0));
 				createNetworkStats()
 			break;
 			case "cluster":
@@ -50,10 +46,10 @@ function InfoPanel() {
 		var connections = APP.dataset.getNetworkStats();
 		var orgConnRatio = Math.round((connections.totalLinkedOrgs/APP.dataset.orgs.length)*100);
 		var prjShareRatio = Math.round((connections.totalSharedPrjs/APP.dataset.prjs.length)*100);
-		$(".network-stats-orgs .stats-caption").html("Networked organisations: "+orgConnRatio+"%");
+		$(".network-stats-orgs .stats-caption span").html(orgConnRatio+"%");
 		$(".network-stats-orgs .stats-paragraph").html(orgConnRatio+"% of organisations in the DSI network are connected to at least one other organisation in the network.");
 		$(".network-stats-orgs .stats-bar .color").transition({width: orgConnRatio+"%"}, 2000);
-		$(".network-stats-prjs .stats-caption").html("Collaborative projects: "+prjShareRatio+"%");
+		$(".network-stats-prjs .stats-caption span").html(prjShareRatio+"%");
 		$(".network-stats-prjs .stats-paragraph").html(prjShareRatio+"% of projects are linked to two or more organisations in the network.");
 		$(".network-stats-prjs .stats-bar .color").transition({width: prjShareRatio+"%"}, 2000);
 	}
--- app/js/modules/networkstats.js
+++ app/js/modules/networkstats.js
@@ -8,10 +8,10 @@ function NetworkStats() {
 		var connections = APP.dataset.getNetworkStats();
 		var orgConnRatio = Math.round((connections.totalLinkedOrgs/APP.dataset.orgs.length)*100);
 		var prjShareRatio = Math.round((connections.totalSharedPrjs/APP.dataset.prjs.length)*100);
-		$(".network-stats-orgs .stats-caption").html("Networked organisations: "+orgConnRatio+"%");
+		$(".network-stats-orgs .stats-caption span").html(orgConnRatio+"%");
 		$(".network-stats-orgs .stats-paragraph").html(orgConnRatio+"% of organisations in the DSI network are connected to at least one other organisation in the network.");
 		$(".network-stats-orgs .stats-bar .color").transition({width: orgConnRatio+"%"}, 2000);
-		$(".network-stats-prjs .stats-caption").html("Collaborative projects: "+prjShareRatio+"%");
+		$(".network-stats-prjs .stats-caption span").html(prjShareRatio+"%");
 		$(".network-stats-prjs .stats-paragraph").html(prjShareRatio+"% of projects are linked to two or more organisations in the network.");
 		$(".network-stats-prjs .stats-bar .color").transition({width: prjShareRatio+"%"}, 2000);
 
