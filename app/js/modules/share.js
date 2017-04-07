43f1af091a72af0c291a599755b04f3289d53495
Emilio <e.bondioli@todo.to.it>
2017-04-07 20:32:48 +0200

    Add media query for mobile cta

--- app/js/modules/share.js
+++ app/js/modules/share.js
@@ -31,7 +31,7 @@ function Share() {
   }
 
   function embedPermalink(link){
-    var code = '<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class="embed-container"><iframe src='+link+' style="border:0"></iframe></div>';
+    var code = '<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }@media screen and (max-width: 480px) { .embed-container { padding-bottom: 100%; } }</style><div class="embed-container"><iframe src='+link+' style="border:0"></iframe></div>';
     $("#share-embed input")
       .attr("value", code)
 
